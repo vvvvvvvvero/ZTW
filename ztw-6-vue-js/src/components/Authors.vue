b1238a446820159fe6ff7de07041e21f728025f1
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2024-04-02 22:23:31 +0200

    vue

---
+++ ztw-6-vue-js/src/components/Authors.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "Authors"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
---
+++ ztw-6-vue/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <h1>Hello World</h1>
+    </div>
+  </header>
+</template>
+
+
+<style scoped>
+
+</style>
